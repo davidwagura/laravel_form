1fd02cf558bd4b9dfabe4d86c7e33b8bb518f299
David wanjohi <davidwagura83@gmail.com>
2024-01-26 14:48:25 +0300

    placeholders for title and description

--- resources/views/add-blog-post-form.blade.php
+++ resources/views/add-blog-post-form.blade.php
@@ -23,11 +23,11 @@
                         @csrf
                         <div class="form-group">
                             <label for="exampleInputEmail1">Title</label>
-                            <input type="text" id="title" name="title" class="form-control" required="">
+                            <input type="text" id="title" placeholder="Enter the title" name="title" class="form-control" required="">
                         </div>
                         <div class="form-group">
                             <label for="exampleInputEmail1">Description</label>
-                            <textarea name="description" class="form-control" required=""></textarea>
+                            <textarea placeholder="Enter description here" name="description" class="form-control" required=""></textarea>
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </Form>
